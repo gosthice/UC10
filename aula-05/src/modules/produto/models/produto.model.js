0379e65d9cb05ae39c6035e2d71793d94469501b
RAILANE SIQUEIRA SERAFIM <a95367729@rn.senac.local>
2025-07-15 15:00:33 -0300

    feat: adicionado conteudo da aula

---
+++ aula-05/src/modules/produto/models/produto.model.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../../../config/configDB');
+
+const ProdutoModel = sequelize.define('Produto', {
+
+    cod_prod:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+    },
+    preco: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    estoque: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+},
+    {
+        tableName: 'produto',
+        createdAt: 'criado_em',
+        updatedAt: 'atualizado_em'
+    },
+
+)
+
+module.exports = ProdutoModel;
---
+++ aula-05/src/modules/tests/produtoController.test.js
@@ -0,0 +1,3 @@
+const { sequelize } = require('../../config/configDB');
+const Produto = require('../produto/models/produto.model');
+const ProdutoController = require('../produto/controllers/produto.controller');
